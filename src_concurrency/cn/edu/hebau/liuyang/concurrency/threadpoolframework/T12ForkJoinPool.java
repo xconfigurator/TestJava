46c974d00506740020bd7bae46c9b915e938b31c
xconfigurator <xconfigurator@hotmail.com>
2018-03-17 07:09:26 +0800

    ForkJoinPool <-- 真爱！

---
+++ src_concurrency/cn/edu/hebau/liuyang/concurrency/threadpoolframework/T12ForkJoinPool.java
@@ -0,0 +1,103 @@
+package cn.edu.hebau.liuyang.concurrency.threadpoolframework;
+
+import java.io.IOException;
+import java.util.Arrays;
+import java.util.Random;
+import java.util.concurrent.ForkJoinPool;
+import java.util.concurrent.RecursiveAction;
+import java.util.concurrent.RecursiveTask;
+
+public class T12ForkJoinPool {
+	static int[] nums = new int[1000000];
+	static final int MAX_NUM = 50000;		// 每个分片不超过50000
+	static Random r = new Random();
+	
+	static {
+		for (int i=0; i < nums.length; i++) {
+			nums[i] = r.nextInt(100);
+		}
+	}
+	
+	// 分治
+	// ForkJoinTask | RecursiveAction 无返回值 | RecursiveTask 有返回值 | CountedCompleter
+	// 演示1 继承RecursiveAction
+	static class AddRecursiveAction extends RecursiveAction {
+		int start, end;
+		
+		AddRecursiveAction(int start, int end) {
+			this.start = start;
+			this.end = end;
+		}
+		
+		// 无返回值
+		@Override
+		protected void compute() {
+			if (end - start <= MAX_NUM) {
+				long sum = 0L;
+				for (int i = start; i < end; i++) {
+					sum += nums[i];
+				}
+				// 没返回值，就只能打印了
+				System.out.println("from:" + start + " to: " + end + " = " + sum);
+			} else {
+				int middle = start + (end - start) / 2;
+				AddRecursiveAction subTask1 = new AddRecursiveAction(start, middle);
+				AddRecursiveAction subTask2 = new AddRecursiveAction(middle, end);
+				subTask1.fork();// 子线程1启动
+				subTask2.fork();// 子线程2启动
+			}
+		}
+	}
+	
+	// 演示2 继承RecursiveTask
+	static class AddRecursiveTask extends RecursiveTask<Long> {
+		
+		int start, end;
+		
+		AddRecursiveTask(int start, int end) {
+			this.start = start;
+			this.end = end;
+		}
+		
+		// 有返回值
+		@Override
+		protected Long compute() {
+			if (end - start <= MAX_NUM) {
+				long sum = 0L;
+				for (int i = start; i < end; i++) {
+					sum += nums[i];
+				}
+				return sum;
+			} else {
+				int middle = start + (end - start) / 2;
+				AddRecursiveTask subTask1 = new AddRecursiveTask(start, middle);
+				AddRecursiveTask subTask2 = new AddRecursiveTask(middle, end);
+				subTask1.fork();// 子线程1启动
+				subTask2.fork();// 子线程2启动	
+				return subTask1.join() + subTask2.join();
+			}
+		}
+	}
+	
+	
+	public static void main(String[] args) throws IOException {
+		// 方法1
+		System.out.println("" + Arrays.stream(nums).sum());// Java Stream API since JDK 8
+		
+		
+		// 方法2 ForkJoinPool
+		ForkJoinPool fjp = new ForkJoinPool(); // 整个递归过程由ForkJoinPool来维护。
+		// 2.1 RecursiveAction
+//		AddRecursiveAction task = new AddRecursiveAction(0, nums.length);
+//		fjp.execute(task);
+		// 2.2 RecursiveTask
+		AddRecursiveTask task = new AddRecursiveTask(0, nums.length);
+		fjp.execute(task);
+		long result = task.join();
+		System.out.println(result);// 49506146
+		
+		// 后台启动守护进程进行计算....
+		// 想要看到输出就需要阻塞
+		System.in.read();
+	}
+}
