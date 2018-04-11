f3e038bec55f79f9636b05d5ed11a32f10cfafaf
xconfigurator <xconfigurator@hotmail.com>
2018-04-12 06:28:06 +0800

    annotation demo

---
+++ src/cn/edu/hebau/liuyang/annotation/Entity.java
@@ -0,0 +1,13 @@
+package cn.edu.hebau.liuyang.annotation;
+
+import static java.lang.annotation.ElementType.TYPE;
+import static java.lang.annotation.RetentionPolicy.RUNTIME;
+
+import java.lang.annotation.Retention;
+import java.lang.annotation.Target;
+
+@Retention(RUNTIME)
+@Target({ TYPE})
+public @interface Entity {
+	String value();
+}
