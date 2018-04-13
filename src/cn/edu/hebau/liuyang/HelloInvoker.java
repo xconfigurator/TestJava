bd1f4a2f9279ccbe53ba8c32bddd3dec27b33340
xconfigurator <xconfigurator@hotmail.com>
2018-04-13 23:51:09 +0800

    try-with-resource

--- src/cn/edu/hebau/liuyang/HelloInvoker.java
+++ src/cn/edu/hebau/liuyang/HelloInvoker.java
@@ -9,16 +9,20 @@ public class HelloInvoker {
 	private static final String CMD = "java -cp F:/workspaces/workspace_oxygen/TestJava/bin cn.edu.hebau.liuyang.Hello";
 
 	public static void main(String[] args) throws IOException {
+		
+		
 		Runtime rt = Runtime.getRuntime();
 		Process process = rt.exec(CMD);
-		
-		BufferedReader reader = new BufferedReader(new InputStreamReader(process.getInputStream()));
-		String info = null;
-		while ((info = reader.readLine()) != null) {
-			System.out.println(info);
-		}
-		
-		reader.close();
+
+		// try-with-resource
+		try (
+			BufferedReader reader = new BufferedReader(new InputStreamReader(process.getInputStream()));
+		){
+			String info = null;
+			while ((info = reader.readLine()) != null) {
+				System.out.println(info);
+			}
+		} 
 	}
 
 }
