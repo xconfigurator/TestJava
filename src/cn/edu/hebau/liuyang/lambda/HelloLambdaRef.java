127dbfcd4011df6b3ee83d08e5c3a6571da3de92
xconfigurator <xconfigurator@hotmail.com>
2018-04-20 14:43:47 +0800

    Lambda

--- src/cn/edu/hebau/liuyang/lambda/HelloLambdaRef.java
+++ src/cn/edu/hebau/liuyang/lambda/HelloLambdaRef.java
@@ -4,6 +4,7 @@ import java.io.PrintStream;
 import java.util.Comparator;
 import java.util.function.BiPredicate;
 import java.util.function.Consumer;
+import java.util.function.Function;
 import java.util.function.Supplier;
 
 /**
@@ -19,6 +20,8 @@ public class HelloLambdaRef {
 		methodRef();
 		System.out.println("######################################");
 		constructorRef();
+		System.out.println("######################################");
+		arrayRef();
 	}
 	
 	// 方法引用
@@ -47,12 +50,17 @@ public class HelloLambdaRef {
 		Supplier<Object> sup = () -> new Object();
 		Supplier<Object> sup2 = Object::new;
 		
-		// 注意并不一定是调用无参构造器。
+		// 注意并不一定是调用无参构造器！
 	}
 	
 	// 数组引用
 	public static void arrayRef() {
-			
+		Function<Integer, String[]> fun = (x) -> new String[x];
+		String[] strs = fun.apply(10);
+		System.out.println(strs.length);
 		
+		Function<Integer, String[]> fun1 = String[]::new;
+		String[] strs1 = fun1.apply(5);
+		System.out.println(strs1.length);
 	}
 }
--- src/cn/edu/hebau/liuyang/lambda/LambdaExercise01.java
+++ src/cn/edu/hebau/liuyang/lambda/LambdaExercise01.java
@@ -3,6 +3,9 @@ package cn.edu.hebau.liuyang.lambda;
 import java.util.Arrays;
 import java.util.Collections;
 import java.util.List;
+import java.util.function.Function;
+
+import cn.edu.hebau.liuyang.lambda.LambdaExercise01.Employee;
 
 /**
  * 练习1： 自定义排序规则
@@ -30,6 +33,12 @@ public class LambdaExercise01 {
 		});
 		
 		emps.forEach(System.out::println);
+		
+		
+		Function<Integer, Employee> fun = (x) -> new Employee(x);
+		Function<Integer, Employee> fun2 = Employee::new;// 这个调用的是一个参数的构造器。
+		System.out.println(fun2.apply(444));
+		
 	}
 
 	static class Employee {
@@ -37,6 +46,11 @@ public class LambdaExercise01 {
 		private int age;
 		private String name;
 		private double salary;
+		
+		public Employee(int id) {
+			super();
+			this.id = id;
+		}
 
 		public Employee(int id, String name,int age, double salary) {
 			super();
